9c11a958575baeae603c0dfabd3bf30ff459d69f
VidraTo <39879137+TommyVidra@users.noreply.github.com>
2019-12-02 20:57:59 +0100

    Add files via upload

---
+++ AdventOfCode2019/Day_1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode2019
+{
+    class Day_1
+    {
+
+        private static void partOne(String[] lines)
+        {
+           
+            int sum = 0;
+            
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                try
+                {
+                    //parses integers for every line in the input
+                    int num = Int32.Parse(lines[i]);
+                    num = num / 3;
+                    num -= 2;
+                    sum += num;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            //displays the sum
+            Console.WriteLine(sum);
+            String s = Console.ReadLine();
+
+        }
+
+        private static void partTwo(String[] lines)
+        {
+            int sum = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                try
+                {
+                    //parses integers for every line in the input
+                    int num = Int32.Parse(lines[i]);
+                    num = num / 3;
+                    num -= 2;
+                    sum += num;
+
+                    while (num > 0)
+                    {
+                        num = num / 3;
+                        num -= 2;
+                        if (num < 0)
+                            num = 0;
+                        sum += num;
+                    }
+                    
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.WriteLine(sum);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            String path = "D:\\Projects\\AdventOfCode2019\\AdventOfCode2019\\AdventOfCode2019\\day_1_input_p1.txt";
+            String[] lines = File.ReadAllLines(path, Encoding.UTF8);
+            
+            //First part
+            partOne(lines);
+
+            //Part two
+            partTwo(lines);
+
+            String s = Console.ReadLine();
+
+        }
+    }
+}
